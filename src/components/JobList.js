1ced36ac292ad217044ee5016ef1bc759ca4d050
mahmoudosman.som@gmail.com <mahmoudosman.som@gmail.com>
2021-01-31 00:32:56 -0600

    nitial commit

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -14,8 +14,6 @@ const JobList = ({ jobTitiles }) => {
     //const cors = "https://cors-anywhere.herokuapp.com/";
     const baseURL =
       "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
-
-    //   const  request = await axios.get(baseURL)
     axios
       .get(baseURL)
       .then((res) => {
